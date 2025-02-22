30039bec21c6cc42baba102e540b41b0dcb7c1ac
biraj-dahal <dahalbiraj10@gmail.com>
2025-02-22 11:30:38 -0500

    todos based on the react boilerplate

---
+++ Kinova Starter/Automated-Package-Classifier/user-interface/robot-dashboard/robot-monitoring-backend.py
@@ -0,0 +1,29 @@
+
+#lets use fastapi?
+
+
+# i have included boilerplate for react component for dashboard, and multiple states alr, feel free to change, add, remove
+
+# TODO: define response models for 
+# robot status, logs -> please see the logging.yaml for the configuration
+# system stats maybe for our computer too? because QRScan sometime malfunctions, and crashes my IDE lol -> cpu usage, memory usage?
+# state, position -> alr defined in the robot class.
+# maybe use the examples from api_python to add more stats, that are built in.
+
+
+
+# TODO: do we use in memory storage, or host a database for WebSocket Connections, and status history? 
+# thoughts? 
+
+
+# TODO: broadcast robot status updates to dashboard realtime.
+
+# TODO: handle requests from the dashboard to the robot too? like stop, home, and anything we wanna add more?
+
+
+
+# TODO: to read the log, either we read the log file, it is under log/robot.log, or we use the logging module directly to read the logs
+
+# TODO: endpoint to retrieve system stats cpu usage, memory usage and maybe also active connections -> dont know if this is possible tho.
+
+
